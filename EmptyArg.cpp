0aab205cc8924909954c56e852407426cbccb27c
Kirill Ivanovsky <100185119+AtomicLyn@users.noreply.github.com>
2024-03-26 10:52:06 +0300

    EmpyArg::TryParse fixe

--- EmptyArg.cpp
+++ EmptyArg.cpp
@@ -6,6 +6,6 @@ using namespace args_parse;
 EmptyArg::EmptyArg(const char* option, const char* longOption, const char* description) : Arg(option, longOption, description) {};
 
 bool EmptyArg::TryParse(const char* arg) {
-	return TryParseOption(arg);
+	return TryParseOption(arg) && strlen(operands) == 0;
 }
 
